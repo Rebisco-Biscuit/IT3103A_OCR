e77226b378fc40ce3f09950c9cf65479330b6b2c
Rebisco-Biscuit <23103808@usc.edu.ph>
2025-04-29 14:10:06 +0800

    all comleted

--- payment-service/payments/src/components/CartPage.jsx
+++ payment-service/payments/src/components/CartPage.jsx
@@ -6,7 +6,7 @@ import EmptyCart from './EmptyCart'; // <- nukes the cart after successful payme
 
 export default function CartPage() {
   const [cartItems, setCartItems] = useState([
-    { id: 'Course006', description: 'Otto', price: '100.00' },
+    { id: 'Course008', description: 'Otto', price: '9999.00' },
   ]);
 
   // Calculate total price dynamically
